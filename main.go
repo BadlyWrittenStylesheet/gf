6eabb22b591bfae60d3fda46c29eee1f7ece3afb
Julian Orlowski <julian.sz.orlowski@gmail.com>
2024-06-22 11:50:09 +0200

    Filter by extension + nice functional approach :)

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ import (
 )
 
 var max_depth int
-var file_name string
+var searchPhrase string
 
 // var file_type string
-// var file_ext string
+
+var file_ext string
 
 type FileNode struct {
 	fs.DirEntry
@@ -25,9 +26,12 @@ type FileNode struct {
 func main() {
 	// var findRecursive = flag.Bool("r", false, "Will find in all subdirectories") // Maybe a -d 3 for max depth?
 	flag.IntVar(&max_depth, "d", 3, "Set the max depth of file checking.")
-	flag.StringVar(&file_name, "n", "", "Search for files with the given phrase in name")
+	flag.StringVar(&searchPhrase, "n", "", "Search for files with the given phrase in name")
+	flag.StringVar(&file_ext, "e", "", "Search for files with a given extension")
+	// flag.StringVar(&file_type, "t", "", "Specify if should search for files or directories")
+
 	flag.Parse()
-	// fmt.Println(max_depth, file_name)
+	// fmt.Println(max_depth, searchPhrase)
 
 	args := flag.Args()
 	if len(args) == 0 {
@@ -71,32 +75,44 @@ func GetDirectoryChildren(dir_path string, current_depth int) ([]FileNode, error
 		return nil, err
 	}
 	for _, file := range dir {
-		// fmt.Println(file.IsDir(), dir_path, file.Name())
 		if current_depth <= max_depth {
 			if file.IsDir() {
-				// fmt.Println("Depth:", current_depth, max_depth)
-				dir_c, err := GetDirectoryChildren(dir_path+"/"+file.Name(), current_depth+1)
+
+				dirChildren, err := GetDirectoryChildren(dir_path+"/"+file.Name(), current_depth+1)
 				if err != nil {
 					return nil, err
 				}
-				// this part skips all directtories that have no children if a file_name is specified and the directory doesn't contain that phrase
-				// fmt.Println(dir_c, file_name == "", strings.Contains(file.Name(), file_name) || len(dir_c) != 0)
-				if file_name == "" {
-					file_list = append(file_list, FileNode{file, dir_c})
-				} else if strings.Contains(file.Name(), file_name) || len(dir_c) != 0 {
-					file_list = append(file_list, FileNode{file, dir_c})
+
+				// log.Println("File: ", file, "children: ", dirChildren, shouldIncludeDir(file, dirChildren))
+				if shouldIncludeDir(file, dirChildren) {
+					file_list = append(file_list, FileNode{file, dirChildren})
 				}
 			} else {
 
-				// this part is responsible for only appending files that contain the file_name content in them
-				// fmt.Printf("filter is: '%s' file name is '%s'\n", file_name, file.Name())
-				if file_name == "" {
-					file_list = append(file_list, FileNode{file, nil})
-				} else if strings.Contains(file.Name(), file_name) {
+				if shouldIncludeFile(file) {
 					file_list = append(file_list, FileNode{file, nil})
 				}
 			}
 		}
 	}
 	return file_list, nil
+}
+
+func shouldIncludeFile(file fs.DirEntry) bool {
+	return filterByName(file) && filterByExtension(file)
+}
+
+func shouldIncludeDir(file fs.DirEntry, children []FileNode) bool {
+	return filterByName(file) || len(children) != 0
+}
+
+func filterByName(file fs.DirEntry) bool {
+	if file_ext != "" && file.IsDir() {
+		return false
+	}
+	return searchPhrase == "" || strings.Contains(file.Name(), searchPhrase)
+}
+
+func filterByExtension(file fs.DirEntry) bool {
+	return file_ext == "" || strings.HasSuffix(file.Name(), "."+file_ext)
 }
